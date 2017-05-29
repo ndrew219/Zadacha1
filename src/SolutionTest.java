78f611a472a5683586fc8ad698577034f0b3c069
mainframe <felix.samsonov@gmail.com>
2017-05-29 22:59:21 +0300

    added junit lib

--- src/SolutionTest.java
+++ src/SolutionTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * Created by mainframe on 29.05.2017.
  */
 public class SolutionTest {
+
     @Test
     public void solution() throws Exception {
         assertTrue(true); // just for fun
