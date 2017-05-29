9abe08bf3a813075afc2edded6f3095599d37984
mainframe <felix.samsonov@gmail.com>
2017-05-29 22:42:27 +0300

    unit test stubs added

---
+++ src/SolutionTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+/**
+ * Created by mainframe on 29.05.2017.
+ */
+public class SolutionTest {
+    @Test
+    public void solution() throws Exception {
+        assertTrue(true); // just for fun
+    }
+
+    @Test
+    public void main() throws Exception {
+        assertTrue(true);
+    }
+}
+
