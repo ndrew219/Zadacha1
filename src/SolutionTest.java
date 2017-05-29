10ce81294b86dc8277272b1f31f596891a427339
mainframe <felix.samsonov@gmail.com>
2017-05-29 23:17:16 +0300

    added a few tests

--- src/SolutionTest.java
+++ src/SolutionTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -8,9 +9,18 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 public class SolutionTest {
 
+    Solution solution = new Solution();
+
     @Test
     public void solution() throws Exception {
-        assertTrue(true); // just for fun
+
+        assertEquals(1, solution.solution(4, 4));
+        assertEquals(2, solution.solution(4, 9));
+        assertEquals(3, solution.solution(4, 17));
+        assertEquals(4, solution.solution(4, 27));
+        assertEquals(5, solution.solution(4, 40));
+        assertEquals(6, solution.solution(4, 50));
+
     }
 
     @Test
